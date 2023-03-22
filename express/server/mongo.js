4b72927e58d357775cc6a578d6fd82fe8a557573
Yous3f <yasilah4@gmail.com>
2023-03-22 10:56:06 +0200

    removed env folder

--- express/server/mongo.js
+++ express/server/mongo.js
@@ -12,6 +12,6 @@ const client = new MongoClient(uri, {
 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+  // perform actions on the collection object ss 
   client.close();
 });
